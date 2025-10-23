fa78276dd169f43a213440a58f124a642a960613
Khushishivkar <shivkarkhushi4@gmail.com>
2025-10-24 01:56:25 +0530

    Update blockchain.py

--- blockchain_project/blockchain.py
+++ blockchain_project/blockchain.py
@@ -6,36 +6,44 @@ from time import time
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.current_donations = []
-
-        # Create genesis block
-        self.new_block(previous_hash='1', proof=100)
+        self.current_transactions = []
+        # Create the genesis block
+        self.new_block(proof=100, previous_hash="1")
 
     def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        """
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
-            'donations': self.current_donations,
+            'transactions': self.current_transactions,
             'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
         }
 
-        # Reset current donations
-        self.current_donations = []
+        # Reset the current list of transactions
+        self.current_transactions = []
+
         self.chain.append(block)
         return block
 
-    def new_donation(self, donor_id, donor_name, amount, organization):
-        self.current_donations.append({
-            'donor_id': donor_id,
-            'donor_name': donor_name,
-            'amount': amount,
-            'organization': organization
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Add a new transaction to the list of transactions
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
         })
         return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
+        """
+        Hash a block
+        """
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
